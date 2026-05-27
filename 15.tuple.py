38b4ee0f384129a0dbdb6c3c034c6e1cbc54ac40
JATIN M DESHMUKH <jatindeshmukh91@gmail.com>
2026-05-27 16:52:38 +0530

    tuple

---
+++ 15.tuple.py
@@ -0,0 +1,3 @@
+my_tuple = (10,100,1000)
+my_tuple += (1000,10000)
+print(my_tuple)
---
+++ 16.tuple.py
@@ -0,0 +1,16 @@
+tuple1 = (1,2,3)
+for element in tuple1:
+    print(element)
+
+tuple2 = (1,2,3,4)
+print(5 in tuple2)
+print(5 not in tuple2)
+
+tuple3 = (1,2,3,4)
+print(len(tuple3))
+print(5 not in tuple3)
+
+tuple4 = tuple1 + tuple2
+tuple5 = tuple3 * 2 
+print(tuple4)
+print(tuple5)
